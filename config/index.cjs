26b4068c1810ada71865527183c5ad08d9cfa2e3
No_Solution <102198634+lzw2004@users.noreply.github.com>
2023-08-23 21:07:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,11 +14,12 @@ const USER_CONFIG = {
   APP_SECRET: '9c37a5b07aa888f404ab09e3b2c219ca',
 
   PROVINCE: '黑龙江',
-  CITY: '肇东',
+  CITY: '绥化',
 
   USERS: [
     {
-      // 想要发送的人的名字
+//**********************************************************************//
+      //第一个要发送的人
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqOka63FbYuDLyiS2CBXxUlnxOLE',
@@ -29,11 +30,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2004', date: '01-16',
+          type: '*生日', name: '美女', year: '2004', date: '01-16',
         },
          // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '李四', year: '2004', date: '05-13',
+          type: '生日', name: '臭货', year: '2004', date: '05-13',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '11-10',
@@ -43,10 +44,42 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-02-05' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    
+    
+//************************************************************************************//
+    //第二个要发送的人
+    {
+      name: '臭货',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oqOka63FbYuDLyiS2CBXxUlnxOLE',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'U7WL5bG8XNi_IxJ9EKiwhswZ9wjcQ5sJR_WZ-D644a8',
+      
+      province: '黑龙江',
+      city: '绥化',
+      
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '06-30',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '美女', year: '2004', date: '01-16',
+        },
+         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '臭货', year: '2004', date: '05-13',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-10',
+        },
+      ],
+        customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-02-05' },
+      ],
+    }
   ],
 
 
