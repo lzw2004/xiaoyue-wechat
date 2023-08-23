385c83829dd1eaf12f1584aa8c5ea0ec3d95a72a
No_Solution <102198634+lzw2004@users.noreply.github.com>
2023-08-23 21:12:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,6 +13,27 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '9c37a5b07aa888f404ab09e3b2c219ca',
 
+   // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+    
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: true,
+    
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: true,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  },
+
   PROVINCE: '黑龙江',
   CITY: '绥化',
 
